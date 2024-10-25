a60d2dfd42ad651527ec1d29aa38262f724760b8
jifvgwhvasesovq <g20220210@gmail.com>
2024-10-25 15:27:18 +0800

    commit update message

--- lib/resty/snowflake.lua
+++ lib/resty/snowflake.lua
@@ -1,9 +1,13 @@
-local ffi = require "ffi"
-local ffi_load = ffi.load
+local ffi = require("ffi")
 local ffi_new = ffi.new
+local ffi_load = ffi.load
 local ffi_string = ffi.string
-local ok, sf = pcall(ffi_load, "snowflake")
-assert(ok, sf)
+local sf_ok, sf = pcall(ffi_load, "snowflake")
+local sf_status, err = assert(sf_ok, string.format("Failed to load the Snowflake C library, error: %s.", sf))
+if not sf_status then
+    ngx.log(ngx.ERR, err)
+    return nil, err
+end
 
 ffi.cdef[[
     typedef struct snowflake{
@@ -33,13 +37,15 @@ function _M.new(self, worker_id, datacenter_id)
         )
     end)
     if not ok then
+        ngx.log(ngx.ERR, err)
         return nil, err
     end
 
     local snowflake = ffi_new("snowflake_t")
     local flag = sf.snowflake_init(snowflake, worker_id, datacenter_id)
     if not flag then
-        return nil, "failed to initialize snowflake"
+        ngx.log(ngx.ERR, string.format("failed to initialize snowflake %s.", flag))
+        return
     end
 
     return setmetatable({ 
@@ -53,7 +59,11 @@ function _M.next_id(self)
     --local ok = sf.snowflake_next_id(self.context, id)
     local id_buf = ffi_new("char[21]")
     local ok = sf.snowflake_next_id(self.context, id_buf, 21)
-    assert(ok, "failed to generate next id")
+    local id_status, err = assert(ok, string.format("failed to generate next id for %s.", id_buf))
+    if not id_status then
+        ngx.log(ngx.ERR, err)
+        return nil, err
+    end
 
     return ffi_string(id_buf)
     --return id[0]
